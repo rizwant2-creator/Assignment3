edb9221dcf788f95cca8a5b7d8906efdab0bd41d
rizwant2-creator <rizwant2@mymacewan.ca>
2026-04-05 04:15:01 +0000

    updated code

--- DB.c
+++ DB.c
@@ -71,3 +71,82 @@ void exportDB(char *fileName){
     fclose(fp);
 }
 
+void freeDB()
+{
+    if (Db==NULL)
+    {
+        return;
+    }
+    if (Db->tableTypeTable!=NULL)
+    {
+        if (Db->tableTypeTable->entries!=NULL)
+        {
+            for (int i=0;i<Db->tableTypeTable->size;i++)
+            {
+               free(Db->tableTypeTable->entries[i].name);
+            }
+            free(Db->tableTypeTable->entries);
+        }
+        free(Db->tableTypeTable);
+    }
+    if (Db->surfaceMaterialTable!=NULL)
+    {
+        if (Db->surfaceMaterialTable->entries!=NULL)
+        {
+            for (int i=0;i<Db->surfaceMaterialTable->size;i++)
+            {
+                free(Db->surfaceMaterialTable->entries[i].name);
+            }
+            free(Db->surfaceMaterialTable->entries);
+        }
+        free(Db->surfaceMaterialTable);
+    }
+    if (Db->structuralMaterialTable!=NULL)
+    {
+        if (Db->structuralMaterialTable->entries!=NULL)
+        {
+            for (int i=0;i<Db->structuralMaterialTable->size;i++)
+            {
+                free(Db->structuralMaterialTable->entries[i].name);
+            }
+            free(Db->structuralMaterialTable->entries);
+        }
+        free(Db->structuralMaterialTable);
+    }
+    if (Db->structuralMaterialTable!=NULL)
+    {
+        if (Db->neighborhoodTable->entries!=NULL)
+        {
+            for (int i=0;i<Db->neighborhoodTable->size;i++)
+            {
+                free(Db->neighborhoodTable->entries[i].name);
+            }
+        free(Db->neighborhoodTable->entries);
+        }
+        free(Db->neighborhoodTable);
+    }
+    if (Db->picnicTableTable!=NULL)
+    {
+        if(Db->picnicTableTable->entries!=NULL)
+        {
+            for (int i=0;i<Db->picnicTableTable->size;i++)
+            {
+                free(Db->picnicTableTable->entries[i].ward);
+                free(Db->picnicTableTable->entries[i].latitude);
+                free(Db->picnicTableTable->entries[i].longlitude);
+                free(Db->picnicTableTable->entries[i].location);
+                free(Db->picnicTableTable->entries[i].street);
+
+            }
+            free(Db->picnicTableTable->entries);
+        }
+        free(Db->picnicTableTable);
+    }
+    free(Db->lineEnding);
+    free(Db);
+    Db=NULL;
+
+
+}
+
+
