1c4ddc893c94a8e9b44c6d67871d8186c704b94f
nguyenr29 <nguyenr29@mymacewan.ca>
2026-04-09 04:06:07 +0000

    countEntries completed and created menu dashboard

--- dashboard.c
+++ dashboard.c
@@ -12,6 +12,8 @@
 //You may include other original headers as you see fit
 #include "DB.h"
 #include "DB_impl.h"
+#include <stdio.h>
+#include <string.h>
 
 int main(int argc, char *argv[]){
 
@@ -38,7 +40,147 @@ int main(int argc, char *argv[]){
     {
         printf("invalid mode. use -c or -b\n");
         printf("\n");
-
+        return 1;
     }
+
+    int option;
+    int memberCode;
+    int total;
+    char fileName[256];
+    char value[256];
+    char memberName[100];
+
+    do{
+        printf("1. Export Database\n");
+        printf("2. Count Entries\n");
+        printf("3. Sort By\n");
+        printf("4. Edit Entry\n");
+        printf("5. Report\n");
+        printf("6. Compress database\n");
+        printf("7. Exit\n");
+        printf("option: ");
+        scanf("%d", &option);
+        getchar();
+
+        if (option == 1){
+            printf("Enter a filename: ");
+            fgets(fileName, sizeof(fileName), stdin);
+            if (fileName[strlen(fileName) - 1] == '\n'){
+                fileName[strlen(fileName) - 1] = '\0';
+            }
+            exportDB(fileName);
+        }
+        else if (option == 2){
+            printf("Enter member code (1.TT 2.SM 3.StM 4.NID 5.NN 6.W): ");
+            scanf("%d", &memberCode);
+            getchar();
+
+            if (memberCode == 1){
+                strcpy(memberName, "table type");
+            } 
+            else if (memberCode == 2){
+                strcpy(memberName, "surface material");
+            } 
+            else if (memberCode == 3){
+                strcpy(memberName, "structural material");
+            }
+            else if (memberCode == 4){
+                strcpy(memberName, "neighbourhood id");
+            }
+            else if (memberCode == 5){
+                strcpy(memberName, "neighbourhood name");
+            }
+            else if (memberCode == 6){
+                strcpy(memberName, "ward");
+            }
+            else {
+                printf("Invalid member code\n");
+                continue;
+            }
+
+            printf("Enter Value: ");
+            fgets(value, sizeof(value), stdin);
+            if (value[strlen(value) - 1] == '\n'){
+                value[strlen(value) - 1] = '\0';
+            }
+            total = countEntries(memberName, value);
+            printf("%s <%s> appeared in the table %d time(s)\n", memberName, value, total);
+        }
+        else if (option == 3){ //sort by
+            printf("Enter a criteria to sort by (1.TT 2.SM 3.StM 4.NID 5.NN 6.W): ");
+            scanf("%d", &memberCode);
+            getchar();
+
+            if (memberCode == 1){
+                //sort by table type
+            }
+            else if (memberCode == 2){
+                //sort by surface material
+            }
+            else if (memberCode == 3){
+                //sort by structural material
+            }
+            else if (memberCode == 4){
+                //sort by neighbourhood id
+            }
+            else if (memberCode == 5){
+                //sort by neighbourhood name
+            }
+            else if (memberCode == 6){
+                //sort by ward
+            }
+            else{
+                printf("Invalid member code\n");
+            }
+
+        }
+        else if (option == 4){ //edit entry
+            int tableID;
+            printf("Enter a numerical table entry to edit: ");
+            scanf("%d", &tableID);
+            getchar();
+
+            printf("Enter member code (1.TT 2.SM 3.StM: ");
+            scanf("%d", &memberCode);
+            getchar();
+
+            printf("Enter Value: ");
+            fgets(value, sizeof(value), stdin);
+            if (value[strlen(value) - 1] == '\n'){
+                value[strlen(value) - 1] = '\0';
+            }     
+        }
+        else if (option == 5){ //report
+            int criteria;
+            printf("Enter a criteria to report by (1. Neighborhood 2. Ward): ");
+            scanf("%d", &criteria);
+            getchar();
+
+            if (criteria == 1){
+                //neighbourhood report
+            }
+            else if (criteria == 2){
+                //ward report
+            }
+            else {
+                printf("Invalid option\n");
+            }
+        }
+        else if (option == 6){ //compress
+            printf("Enter filename: ");
+            fgets(fileName, sizeof(fileName), stdin);
+             if (fileName[strlen(fileName) - 1] == '\n'){
+                fileName[strlen(fileName) - 1] = '\0';
+            }
+
+            //compress database
+        }
+        else if (option != 7){
+            printf("Invalid option\n");
+        }
+    } while (option != 7);
+
+    freeDB();
     return 0;
+
 }
